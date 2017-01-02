263f22b5d1eb7b4cefeaa45bf15677a1a3d617fd
JacoobWang <531532957@qq.com>
2017-01-02 15:14:09 +0800

    Update conf-server.js

--- conf-server.js
+++ conf-server.js
@@ -1,9 +1,3 @@
-﻿/*
- * Configuration of UFT 
- * Created on 2015-11-12 by Manji.z
- * Last Modified at 2015-11-12 by Manji.z
- * Only For Server
- */
 
 ;(function() {
 	'use strict';
@@ -12,14 +6,6 @@
 	 var conf = function(obj) {
 	 	return {
 	 		port: 8000 || process.env.PORT,
-	 		nodemailer: {
-			    service: '163',
-			    auth: {
-			        user: '***',
-			        pass: '***'
-			    }
-			},
-			// 攸关邮件链接等
 			referer: 'http://localhost/',
 			tempPath: 'temp',
 			uploadPath: 'image-upload/upload',
